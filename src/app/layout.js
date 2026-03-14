9d5b2e3ae2490976530447d553cf52f575b42433
Suryamani kumar <105836069+suryamani1kumar@users.noreply.github.com>
2026-03-14 08:27:15 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,7 @@ import "./globals.css";
 
 export const metadata = {
   title: "Travel Blisss",
-  description: "Travel Blisss
+  description: "Travel Blisss"
 };
 
 export default function RootLayout({ children }) {
